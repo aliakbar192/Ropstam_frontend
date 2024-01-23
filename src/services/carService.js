8398362ced3bd93688443e7cdd57e47822901a2a
aliakbar92 <ali.akbar@axcelerate.ai>
2024-01-23 05:56:34 +0500

    resolve auth header bug

--- src/services/carService.js
+++ src/services/carService.js
@@ -8,34 +8,35 @@ const apiEndpoint = `${apiUrl}/car`;
 const jwtToken = authService.getJwt();
 console.log('jwt token', jwtToken);
 
-// Setting the JWT token for authenticated requests
-http.setJwt(jwtToken);
-
 // Function to create a new car entry
 async function createCarData(carData) {
+    console.log('jwt', jwtToken);
+    http.setJwt(jwtToken);
     return await http.post(`${apiEndpoint}/createCar`, carData);
 }
 
 // Function to get all cars for a specific user
 async function getAllCarByUser(data) {
+    http.setJwt(jwtToken);
     return await http.post(`${apiEndpoint}/getAllCarsByUserId`, data);
 }
 
 // Function to delete a car entry by ID
 async function deleteCarById(id) {
+    http.setJwt(jwtToken);
     console.log(id);
     return await http.delete(`${apiEndpoint}/${id}`);
 }
 
 // Function to get details of a single car by ID
 async function getOneCarById(id) {
-    console.log(id);
+    http.setJwt(jwtToken);
     return await http.get(`${apiEndpoint}/${id}`);
 }
 
 // Function to update a car entry by ID with new data
 async function updateCar(id, body) {
-    console.log(id);
+    http.setJwt(jwtToken);
     return await http.put(`${apiEndpoint}/${id}`, body);
 }
 
